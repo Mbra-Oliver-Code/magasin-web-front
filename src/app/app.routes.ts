372ae40a8e9a2ae3b3fa837eeeff49a329d65e8b
Mbra Oliver <96344566+Mbra-Oliver@users.noreply.github.com>
2024-12-06 20:19:38 +0000

    Install tailwind css & first page creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard.component').then(
+        (module) => module.DashboardComponent
+      ),
+  },
+];
